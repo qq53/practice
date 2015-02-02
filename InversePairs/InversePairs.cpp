32a71b442ab5769219439d74a54945259f97598e
Sen1993 <gsen1993@gmail.com>
2015-02-03 04:14:54 +0800

    数组中逆序数对

--- InversePairs/InversePairs.cpp
+++ InversePairs/InversePairs.cpp
@@ -2,7 +2,7 @@
 /*
   ------------------------------------
   Create date : 2015-02-02 23:35
-  Modified date: 2015-02-02 23:38
+  Modified date: 2015-02-03 04:14
   Author : Sen1993
   Email : gsen1993@gmail.com
   ------------------------------------
@@ -12,11 +12,49 @@
 
 using namespace std;
 
+int InversePairsCore(int *data, int *copy, int start, int end){
+	if(start == end){
+		copy[start] = data[start];
+		return 0;
+	}
+
+	int len = (end-start)/2;
+	int left = InversePairsCore(copy, data, start, start+len);
+	int right = InversePairsCore(copy, data, start+len+1, end);
+
+	int i = start+len;
+	int j = end;
+	int count = 0;
+	int copyIndex = end;
+	while(i >= start && j >= start+len+1){
+		if(data[i] > data[j]){
+			copy[copyIndex--] = data[i--];
+			count += j - start - len;
+		}else{
+			copy[copyIndex--] = data[j--];
+		}
+	}
+
+	for(; i >= start; --i)
+		copy[copyIndex--] = data[i];
+	for(; j >= start+len+1; --j)
+		copy[copyIndex--] = data[j];
+
+	return left + right + count;
+}
+
 //求数组中逆序对（前面的数比后面数大的对）的个数 比如7,5,6,4这个数组有5个逆序对
 void InversePairs(int *data, int len){
 	if(data == NULL || len <= 0)
 		throw myException("params error");
 
+	int *copy = new int[len];
+	for(int i = 0; i < len; ++i)
+		copy[i] = data[i];
+
+	int count = InversePairsCore(data, copy, 0, len-1);
+	delete [] copy;
+	cout << count << endl;
 }
 
 int main(){
