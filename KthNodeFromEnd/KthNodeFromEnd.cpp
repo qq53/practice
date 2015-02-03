3f10f4fd68930bcecaf7165cbbc82aed9ea494a9
Sen1993 <gsen1993@gmail.com>
2015-02-04 01:09:49 +0800

    修改库的改变 单链表倒数第n个结点

--- KthNodeFromEnd/KthNodeFromEnd.cpp
+++ KthNodeFromEnd/KthNodeFromEnd.cpp
@@ -2,7 +2,7 @@
 /*
   ------------------------------------
   Create date : 2015-01-18 03:51
-  Modified date: 2015-01-18 05:10
+  Modified date: 2015-02-04 01:09
   Author : Sen1993
   Email : gsen1993@gmail.com
   ------------------------------------
@@ -15,37 +15,36 @@
 using namespace std;
 
 //采用2个指针 快慢指针来解决,两个指针间隔n,当快指针到尾时,慢指针就是倒数n结点的指针
-void FindKthToTail(cListNode *c_pHead, int n){
-	if(c_pHead == NULL || n <= 0)
+void FindKthToTail(ListNode *pHead, int n){
+	if(pHead == NULL || n <= 0)
 		throw myException("FindKthToTail fail");
 
-	ListNode *pAhead = c_pHead->getHead();
+	ListNode *pAhead = pHead;
 	ListNode *pBehind = pAhead;
 
 	for(int i = 0; i < n - 1; ++i){
-		pAhead = c_pHead->next(pAhead);
+		pAhead = pAhead->m_pNext;
 		if(pAhead == NULL){
 			cout << "list too short" << endl;
 			return;
 		}
 	}
 
-	while(c_pHead->next(pAhead) != NULL){
-		pAhead = c_pHead->next(pAhead);
-		pBehind = c_pHead->next(pBehind);
+	while(pAhead->m_pNext != NULL){
+		pAhead = pAhead->m_pNext;
+		pBehind = pBehind->m_pNext;
 	}
 
-	cout << "Find this ListNode value: " << c_pHead->getValue(pBehind) << endl;
+	cout << "Find this ListNode value: " << pBehind->m_nValue << endl;
 }
 
 int main(){
-	cListNode *c_pListNode = new cListNode;
 	int array[] = {1,2,3,4,5,6};
 	int len = sizeof(array)/sizeof(array[0]);
 
 	try{
-		c_pListNode->create(array, len);
-		FindKthToTail(c_pListNode, 3);
+		ListNode *pHead = CreateLists(array, len);
+		FindKthToTail(pHead, 3);
 	}catch(myException &e){
 		cout << e.what() << endl;
 	}
