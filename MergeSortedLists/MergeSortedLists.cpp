9a77563e7489a14cec2823a5a80416c6f23dd43d
Sen1993 <gsen1993@gmail.com>
2015-02-04 01:06:01 +0800

    修改库改变 合并链表

--- MergeSortedLists/MergeSortedLists.cpp
+++ MergeSortedLists/MergeSortedLists.cpp
@@ -2,7 +2,7 @@
 /*
   ------------------------------------
   Create date : 2015-01-19 02:15
-  Modified date: 2015-01-19 02:42
+  Modified date: 2015-02-04 01:03
   Author : Sen1993
   Email : gsen1993@gmail.com
   ------------------------------------
@@ -14,49 +14,46 @@
 
 using namespace std;
 
-cListNode *MergeSortedLists(cListNode *c_p1, cListNode *c_p2){
-	ListNode *p1 = c_p1->next(c_p1->getHead());
-	ListNode *p2 = c_p1->next(c_p2->getHead());
+ListNode *MergeSortedLists(ListNode *pHead1, ListNode *pHead2){
+	ListNode *p1 = pHead1->m_pNext;
+	ListNode *p2 = pHead2->m_pNext;
 
-	if( p1 == NULL || p2 == NULL)
+	if( p1 == NULL || p2 == NULL )
 		throw myException("empty list");
 
-	cListNode *c_p3 = new cListNode;
-	ListNode *p3 = new ListNode;
-	c_p3->setHead(p3);
+	ListNode *pHead3 = new ListNode;
+	ListNode *p3 = pHead3;
 
 	while(p1 != NULL && p2 != NULL){
-		if(c_p1->getValue(p1) < c_p2->getValue(p2)){
-			c_p3->setNext(p3, p1);
-			p1 = c_p1->next(p1);
+		if(p1->m_nValue < p2->m_nValue){
+			p3->m_pNext = p1;
+			p1 = p1->m_pNext;
 		}else{
-			c_p3->setNext(p3, p2);
-			p2 = c_p2->next(p2);
+			p3->m_pNext = p2;
+			p2 = p2->m_pNext;
 		}
-		p3 = c_p3->next(p3);
+		p3 = p3->m_pNext;
 	}
 	if(p1 != NULL){
-		c_p3->setNext(p3, p1);
+		p3->m_pNext = p1;
 	}else if( p2 != NULL){
-		c_p3->setNext(p3, p2);
+		p3->m_pNext = p2;
 	}
 
-	return c_p3;
+	return pHead3;
 }
 
 int main(){
-	cListNode *c1 = new cListNode;
-	cListNode *c2 = new cListNode;
 	int array1[] = {1,3,5};
 	int len1 = sizeof(array1)/sizeof(array1[0]);
 	int array2[] = {2,4,6};
 	int len2 = sizeof(array2)/sizeof(array2[0]);
 	
 	try{
-		c1->create(array1, len1);
-		c2->create(array2, len2);
-		cListNode *c3 = MergeSortedLists(c1, c2);
-		c3->print();
+		ListNode *pHead1 = CreateLists(array1, len1);
+		ListNode *pHead2 = CreateLists(array2, len2);
+		ListNode *pHead3 = MergeSortedLists(pHead1, pHead2);
+		PrintLists(pHead3);
 	}catch(myException &e){
 		cout << e.what() << endl;
 	}
